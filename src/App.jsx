7ad4f8f508bf3ae37953dbfe8485a177b47588f3
nicoelias997 <nicoelias997@gmail.com>
2022-10-07 18:39:40 +0200

    Elimino y limpio archivos que no usare

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+   <Container>
+    <h1>Hola mundo</h1>
+   </Container>
+  );
+}
+
+export default App;
